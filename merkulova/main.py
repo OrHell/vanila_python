5012ce937d1cbc14bdbe56729796b696d5bd4911
OrHell <43348516+OrHell@users.noreply.github.com>
2019-02-17 15:46:20 +0300

    Add files via upload

---
+++ merkulova/main.py
@@ -0,0 +1,42 @@
+import zipfile
+import os
+import time
+import webbrowser	
+def zip_direct():
+	source_ku = input ('Куда отправить архив:')
+	source_ot  = input ('Откуда брать архив:')
+	chislo = int(input('Time:'))
+	i=0
+	col_vo =int(input('ColVo:')) #добавить сколько раз повторить
+	while i<col_vo:
+		fantasy_zip = zipfile.ZipFile(source_ku, 'w')
+		for folder, subfolders, files in os.walk(source_ot):
+			for file in files:
+				fantasy_zip.write(os.path.join(folder, file), os.path.relpath(os.path.join(folder,file), source_ot), compress_type = zipfile.ZIP_DEFLATED)      
+		fantasy_zip.close()
+		i=i+1
+		#Добавить условие выхода из цикла 
+		print (i,'- Архив создан')
+		time.sleep(chislo)
+	return main()
+def main():	
+	print('==============================')
+	print('            MENTALL           ')
+	print('==============================')
+	user_name = input ('Username:')
+	user_password = input('Password:')
+	print('==============================')
+	print('            ENDLINE            ')
+	print('==============================')
+	if user_name == '2584' and user_password == '2584':
+		return zip_direct()
+	else:
+		return main()
+if __name__ == '__main__':
+	main()
+       
+
+	 
+			
+	 
+			    
