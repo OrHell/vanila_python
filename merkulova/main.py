2683a8c8b437d1586321b61769265111b4590653
OrHell <43348516+OrHell@users.noreply.github.com>
2019-02-17 16:14:10 +0300

    Add files via upload

--- merkulova/main.py
+++ merkulova/main.py
@@ -3,13 +3,14 @@ import os
 import time
 import webbrowser	
 def zip_direct():
+	name_archive = input('Name:')
 	source_ku = input ('Куда отправить архив:')
 	source_ot  = input ('Откуда брать архив:')
 	chislo = int(input('Time:'))
 	i=0
-	col_vo =int(input('ColVo:')) #добавить сколько раз повторить
+	col_vo =int(input('ColVo:')) 
 	while i<col_vo:
-		fantasy_zip = zipfile.ZipFile(source_ku, 'w')
+		fantasy_zip = zipfile.ZipFile(source_ku+name_archive+'.zip', 'w')
 		for folder, subfolders, files in os.walk(source_ot):
 			for file in files:
 				fantasy_zip.write(os.path.join(folder, file), os.path.relpath(os.path.join(folder,file), source_ot), compress_type = zipfile.ZIP_DEFLATED)      
