9903377a9c0a1ed4e1ea20bd952a85540481cd1c
OrHell <43348516+OrHell@users.noreply.github.com>
2020-01-20 21:00:09 +0300

    Add files via upload

--- pain.py
+++ pain.py
@@ -6,22 +6,23 @@ from xgboost import XGBClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
-df=pd.read_csv('parkinsons.data')
-df.head()
+data_file=pd.read_csv('parkinsons.data')
+data_file.head()
 
-
-features=df.loc[:,df.columns!='status'].values[:,1:]
-labels=df.loc[:,'status'].values
-print(labels[labels==1].shape[0], labels[labels==0].shape[0])
-
+features=data_file.loc[:,data_file.columns!='status'].values[:,1:]
+labels=data_file.loc[:,'status'].values
 
 scaler=MinMaxScaler((-1,1))
 x=scaler.fit_transform(features)
 y=labels
 
 x_train,x_test,y_train,y_test=train_test_split(x, y, test_size=0.2, random_state=7)
-model=XGBClassifier()
+model_data=XGBClassifier()
 
-model.fit(x_train,y_train)
-y_pred=model.predict(x_test)
-print(accuracy_score(y_test, y_pred)*100)
+model_data.fit(x_train,y_train)
+y_pred=model_data.predict(x_test)
+
+pred_mode = accuracy_score(y_test, y_pred)*100
+
+print("Процент Статуса болезни (Здоров)",accuracy_score(y_test, y_pred)*100)
+print("Процент Статуса болезни (Болен)",100-(accuracy_score(y_test, y_pred)*100))
